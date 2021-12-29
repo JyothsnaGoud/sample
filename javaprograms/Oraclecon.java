de375520c936bbd5fb8554c5afa187093e386191
JyothsnaGoud <jyothsna.goud@nucleusteq.com>
2021-12-29 19:57:19 +0530

    Added some more programs

---
+++ javaprograms/Oraclecon.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+public class Oraclecon
+{
+public static void main(String args[])
+{
+/*try
+{
+Class.forName("oracle.jdbc.driver.OracleDriver");
+Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","xe","oracle");
+Statement stmt=con.createStatement();
+ResultSet rs=stmt.executeQuery("select * from emp");
+while(rs.next())
+System.out.println(rs.getInt(1)+" "+rs.getString(2));
+con.close();
+}
+catch(Exception e){
+System.out.println(e);
+}*/
+}
+}
